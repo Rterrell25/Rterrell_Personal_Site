f4de3804a515bdc89f20102cf155343d0e0997d9
Rob Terrell <Rterrell25@gmail.com>
2019-12-12 10:22:54 -0500

    udated home screen

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a91418f1779ef07f3735";
+/******/ 	var hotCurrentHash = "92e9985f1809fa4f9f05";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
