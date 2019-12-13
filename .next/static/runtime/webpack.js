05a3d66da7cbaebef2b5858df2e4d8bfd5e382eb
Rob Terrell <Rterrell25@gmail.com>
2019-12-13 15:17:00 -0500

    please work

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "139feb21ae2d4186d997";
+/******/ 	var hotCurrentHash = "6ea6f074e931a3310e8b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
