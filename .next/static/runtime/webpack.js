7815f543f153755c7c6b942deecf93a8c83f96fb
Rob Terrell <Rterrell25@gmail.com>
2019-12-13 13:45:14 -0500

    completed tasks for issue 12

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f9c3e3406ae8c449dfde";
+/******/ 	var hotCurrentHash = "541d4e17b040143326b5";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ pages/portfolio.js
@@ -0,0 +1,55 @@
+<div className={css.row}>
+  <div className={css.column}>
+    <div>
+      <Link href="/oddjobs">
+        <div className={css.hovereffect}>
+          <img
+            className={css.imgresponsive}
+            src="https://i.imgur.com/Tojgmv8.png"
+            alt=""
+          />
+
+          <div className={css.overlay}>
+            <h2>Click For Details</h2>
+          </div>
+        </div>
+      </Link>
+    </div>
+  </div>
+
+  <div className={css.column}>
+    <div>
+      <Link href="/barhop">
+        <div className={css.hovereffect}>
+          <img
+            className={css.imgresponsive}
+            src="https://i.imgur.com/j8GO18n.jpg"
+            alt=""
+          />
+
+          <div className={css.overlay}>
+            <h2>Click For Details</h2>
+          </div>
+        </div>
+      </Link>
+    </div>
+  </div>
+
+  <div className={css.column}>
+    <div>
+      <Link href="/todo">
+        <div className={css.hovereffect}>
+          <img
+            className={css.imgresponsive}
+            src="https://i.imgur.com/muz1gcq.png"
+            alt=""
+          />
+
+          <div className={css.overlay}>
+            <h2>Click For Details</h2>
+          </div>
+        </div>
+      </Link>
+    </div>
+  </div>
+</div>;
