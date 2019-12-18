34e2a2c641130208fcdd4eb1ec78eea2d16477eb
Rob Terrell <Rterrell25@gmail.com>
2019-12-18 13:09:15 -0500

    have fixed image sizing issues with larger screens

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "3fb24e71fa521e92819a";
+/******/ 	var hotCurrentHash = "08f650f966456a44277b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
